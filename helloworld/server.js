da454d5b0e1c65bfcaefa0295740b5bbf33d58d9
Christian Duarte <cduarte3@myseneca.ca>
2022-09-10 14:50:34 -0400

    updated declaration comments

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+* BTI325 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+* 
+* Name: Christian Duarte Student ID: 158207208 Date: September 10th 2022
+*
+* Online (Cyclic) URL: 
+* https://different-threads-cow.cyclic.app/
+*
+********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
